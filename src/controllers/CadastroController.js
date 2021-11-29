12e1882f45c8b48dd9d127ccfc934a3c28737d0c
Rafael Alves do Nascimento Oliveira <faelalvesoliveira06@gmail.com>
2021-11-29 15:47:29 -0300

    Criação dos arquivos validacpf e validaform

--- src/controllers/CadastroController.js
+++ src/controllers/CadastroController.js
@@ -1,9 +1,10 @@
+// let validaCpf = require('')
 const CadastroController =  {
     // index = método do controller para renderizar uma view, chamado em index.js
     index: (req, res) => {
         // indica o arquivo EJS dentro de view a ser chamado
         return res.render('cadastro', {
-            arquivoCss: 'cadastro.css'
+            arquivoCss: 'cadastro.css',
         });
     }
 }

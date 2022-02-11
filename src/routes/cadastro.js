9628b730a138b660965e4722cd4b89d324ab4a7b
Daniel Rocha <dgustavo@id.uff.br>
2022-02-10 22:42:17 -0300

    add working register functions

--- src/routes/cadastro.js
+++ src/routes/cadastro.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const CadastroController = require('../controllers/CadastroController');
 
 // GET Cadastro Page
-router.get('/', CadastroController.index);
+router.get('/', CadastroController.showRegister);
+
+// POST Enviando os dados do formulário para cadastro do usuário
+router.post('/', CadastroController.register);
 
 module.exports = router;

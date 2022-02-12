9eb5f35dbe4bddc4b1dd24b976849f1ae81d53ed
Daniel Rocha <dgustavo@id.uff.br>
2022-02-12 08:42:07 -0300

    add loggedUser info on NavBar

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -1,8 +1,3 @@
-const bcrypt = require('bcryptjs');
-const validateRegister = require('../middlewares/validator');
-
-
-
 const UsuarioController = {
   
   index: (req, res) => {
@@ -16,40 +11,7 @@ const UsuarioController = {
     }
     res.render('usuario');
   },
-
-  // salvaCadastro: (req, res) => {
-  //   let {nome, email, senha} = req.body;
-  //   let senhaC = bcrypt.hashSync(senha, 10)
-  //   let usuario = JSON.stringify({nome, email, senha:senhaC});
-    
-  //   fs.writeFileSync(usuarioJson, usuario)
-  //   res.send('Usuário cadastrado com sucesso!');
-  // },
-
-  loginForm: (req, res) => {
-    res.render('login')
-  },  
-  logarUsuario: (req, res) => {
-    let {email, senha} = req.body;
-    let usuarioSalvo = fs.readFileSync(usuarioJson,{encoding: 'utf-8'});
-    usuarioSalvo = JSON.parse(usuarioSalvo);
-
-    if(email != usuarioSalvo.email) {
-      return res.send('Usuário inválido!');
-    }
-
-    if(bcrypt.compareSync(senha, usuarioSalvo.senha)){
-      return res.send("Senha inválida!");
-    }
-
-    req.session.usuario = usuarioSalvo
-
-    if(logado != undefined){
-      res.cookie('usuarioLogado', usuarioSalvo.email, {maxAge:600000})
-    }
-
-    res.redirect("/produtos");
-  }     
+   
 }
 
 module.exports = UsuarioController;

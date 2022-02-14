c27ce105f7e79c481ea3e8ddfe73a97f18820aad
Daniel Rocha <dgustavo@id.uff.br>
2022-02-14 11:10:14 -0300

    add heroku settings

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,19 @@
 const config = {
-  username: 'root',
-  password: 'root',
-  database: 'meu_home_office',
-  host: 'localhost',
+  username: 'ba461de7b1954d',
+  password: '8489131f',
+  database: 'heroku_30117ced7ab3e10',
+  host: 'us-cdbr-east-05.cleardb.net',
   dialect: 'mysql',
 };
 
 module.exports = config;
+
+//Example
+//mysql://b54966f5205365:9cbaa662@eu-cdbr-west-01.cleardb.com/heroku_b1791a6475f6a54?reconnect=true
+
+//We need to break this down and get the following details:
+
+//Username	b54966f5205365
+//Password	9cbaa662
+//Host	eu-cdbr-west-01.cleardb.com
+//Database	heroku_b1791a6475f6a54

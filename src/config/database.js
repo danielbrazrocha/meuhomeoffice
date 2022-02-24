3e66a45f7620dce094d2039cef33e618c667ff93
Daniel Rocha <dgustavo@id.uff.br>
2022-02-23 23:22:26 -0300

    back to heroku settings

--- src/config/database.js
+++ src/config/database.js
@@ -1,48 +1,24 @@
-module.exports = {
-    development: {
-        username: "root",
-        password: "root",
-        database: "meu_home_office",
-        host: "localhost",
-        dialect: "mysql",
-    },
-    production: {
-      use_env_variable: 'DATABASE_URL',
-      dialect: "mysql",
-    //   protocol: 'postgres',
-    //   dialectOptions: {
-    //     ssl: { // https://github.com/sequelize/sequelize/issues/12083
-    //       require: true,
-    //       rejectUnauthorized: false,
-    //     },
-    //   },
-    },
-  };
 
+const config = {
+  username: 'root',
+  password: 'root',
+  database: 'meu_home_office',
+  host: 'localhost',
+  username: 'ba461de7b1954d',
+  password: '8489131f',
+  database: 'heroku_30117ced7ab3e10',
+  host: 'us-cdbr-east-05.cleardb.net',
+  dialect: 'mysql',
+};
 
-// module.exports = {
-//     development: {
-//       username: "root",
-//       password: "root",
-//       database: "meu_home_office",
-//       host: "localhost",
-//       dialect: "mysql",
-//     },
-//     test: {
-//         username: process.env.DB_USER,
-//         password: process.env.DB_PWD,
-//         database: process.env.DB_NAME,
-//         host: process.env.DB_HOST,
-//         port: process.env.DB_PORT,
-//         dialect: process.env.DB_DIALECT,
-//         logging: true
-//     },
-//     production: {
-//         username: process.env.DB_USER,
-//         password: process.env.DB_PWD,
-//         database: process.env.DB_NAME,
-//         host: process.env.DB_HOST,
-//         port: process.env.DB_PORT,
-//         dialect: process.env.DB_DIALECT
-//     }
-// };
+module.exports = config;
+
+//Example
+//mysql://b54966f5205365:9cbaa662@eu-cdbr-west-01.cleardb.com/heroku_b1791a6475f6a54?reconnect=true
+
+//We need to break this down and get the following details:
+
+//Username	b54966f5205365
+//Password	9cbaa662
+//Host	eu-cdbr-west-01.cleardb.com
+//Database	heroku_b1791a6475f6a54 

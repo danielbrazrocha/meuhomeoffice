d2b910c27f072f5f3ed15db34dff96604c266ac5
Daniel Rocha <dgustavo@id.uff.br>
2022-02-23 23:03:04 -0300

    add function to delete a product

--- src/routes/produto.js
+++ src/routes/produto.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 const ProdutoController = require('../controllers/ProdutoController');
 const isAuth = require('../middlewares/isAuth');
+const isAdmin = require('../middlewares/isAdmin');
 
-// GET Posts Page
+// GET Product Details Page
 router.get('/:id', isAuth, ProdutoController.detalhesProduto);
+
+// GET to Delete Product
+router.get('/deletar/:id', isAdmin, ProdutoController.deletarProduto);
 
 module.exports = router;
